dec046e8f7f33636dcdbb63ec603b16508860da4
federicofanini <federico.fanini9@gmail.com>
2025-09-28 10:35:24 +0200

    hero

---
+++ src/components/logo.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+export const Logo = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={cn("flex items-center gap-1 font-semibold text-xl", className)}
+    >
+      Locked
+      <span className="bg-primary text-primary-foreground rounded-xs px-1.5 py-[1px] font-bold">
+        in
+      </span>
+    </div>
+  );
+};
