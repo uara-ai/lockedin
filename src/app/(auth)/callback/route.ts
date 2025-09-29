81ae49562b3bebe3301888ffaf537e99ae503f37
federicofanini <federico.fanini9@gmail.com>
2025-09-29 12:40:37 +0200

    callback

--- src/app/(auth)/callback/route.ts
+++ src/app/(auth)/callback/route.ts
@@ -2,4 +2,4 @@ import { handleAuth } from "@workos-inc/authkit-nextjs";
 
 // Redirect the user to `/` after successful sign in
 // The redirect can be customized: `handleAuth({ returnPathname: '/foo' })`
-export const GET = handleAuth();
+export const GET = handleAuth({ returnPathname: "/feed" });
